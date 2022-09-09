7d554f771d52bb0f63d0ad567c0954bf87caddc8
carolina-ml <cmarinleal@gmail.com>
2022-09-09 19:58:03 -0300

    learning a bit of .java

---
+++ Main.java
@@ -0,0 +1,31 @@
+/** 
+to declare a variable we use the syntax type name_of_the_varible = value;
+
+Operadores Lógicos:
+
+== 
+
+&&
+
+|| 
+
+!=
+& 
+
+se (verdadeiro & verdadeiro)
+le tudo
+se (falso & verdadeiro)
+ele pula o primeiro falso e vai pro próximo bloco de código 
+
+*/
+
+public class Main{
+    public static void main(String[] args){
+    String name = "Pedro";
+    int years_old = 25;
+    double salary = 1923.00;
+    boolean married = true;
+
+System.out.println("Name: " + name +"\nyears_old: "+ years_old +"\nSalary: "+ salary +"\nmarried: "+ married);    
+    }
+} 
